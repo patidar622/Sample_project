84672191588a915800b2fc6fb4bf58c44248ad36
vishal patidar <vishalpatidarjma7@gmail.com>
2021-06-02 21:38:03 +0530

    last last last

--- src/main/java/com/example/demo/Hello_vishal.java
+++ src/main/java/com/example/demo/Hello_vishal.java
@@ -5,6 +5,7 @@ public class Hello_vishal {
     public void hello()
     {
         System.out.println("Hello Vishal This is for sample");
+         System.out.println("Hello Vishal This is for sample1");
         
     }
 }
