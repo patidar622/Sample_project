55b0fc97d869d68b633ae020f58a3bf3d3232b8f
vishal patidar <vishalpatidarjma7@gmail.com>
2021-06-02 15:35:45 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Hello_vishal.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.Scanner;
+import java.io.FileReader;
+public class Hello_vishal {
+    public void hello()
+    {
+        System.out.println("Hello Vishal This is for sample");
+    }
+}
