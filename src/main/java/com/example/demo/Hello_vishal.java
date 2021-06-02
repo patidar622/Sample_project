1e9ae5e4163c6d5b95c0ea5618f41749c046b7fb
vishal patidar <vishalpatidarjma7@gmail.com>
2021-06-02 21:27:40 +0530

    Update Hello_vishal.java

--- src/main/java/com/example/demo/Hello_vishal.java
+++ src/main/java/com/example/demo/Hello_vishal.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import java.util.Scanner;
 public class Hello_vishal {
     public void hello()
     {
         System.out.println("Hello Vishal This is for sample");
+        System.out.println("Hello Vishal This is for sample2");
+        System.out.println("Hello Vishal This is for sample3");
+        System.out.println("Hello Vishal This is for sample4");
     }
 }
