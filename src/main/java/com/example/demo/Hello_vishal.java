0f59c1e53063d4886a9db3ed85dfafb6b446ce4a
vishal patidar <vishalpatidarjma7@gmail.com>
2021-06-02 21:33:41 +0530

    hii last one

--- src/main/java/com/example/demo/Hello_vishal.java
+++ src/main/java/com/example/demo/Hello_vishal.java
@@ -5,8 +5,6 @@ public class Hello_vishal {
     public void hello()
     {
         System.out.println("Hello Vishal This is for sample");
-        System.out.println("Hello Vishal This is for sample2");
-        System.out.println("Hello Vishal This is for sample3");
-        System.out.println("Hello Vishal This is for sample4");
+        
     }
 }
